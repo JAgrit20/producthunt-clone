6d3afd56ec3b0b3eed8e06e1680256015494d2ef
JAgrit20 <jagritacharya2020@gmail.com>
2020-12-08 22:58:23 +0530

    upvote functionality added

--- producthunt/products/views.py
+++ producthunt/products/views.py
@@ -37,3 +37,12 @@ def details(request,product_id):
     product = get_object_or_404(Product,pk=product_id)
 
     return render(request,'products/detail.html',{'product':product})
+    
+@login_required(login_url="/accounts/signup")
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect('/products/' + str(product.id))
+   
